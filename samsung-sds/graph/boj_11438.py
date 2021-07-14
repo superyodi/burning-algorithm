321e5335f79d0ddd5bb557e8363cb831ba95dd0d
superyodi <songagreen@gmail.com>
2021-07-14 17:06:25 +0900

    samsung-sds 백준 LCA2 문제 해결?
    LCA를 응용해서 2의 n승씩 건너뛰면서 부모 노드를 찾아가야하는 문제인 LCA2를 풀었다.
    문제는 풀었지만 사실 코드가 어떻게 돌아가는지는 잘 모르겠다.
    parents[n][i] 형식으로 n의 2의 i제곱 위의 조상들을 저장하고 찾는 과정을 완벽하게 이해하는건 실패했지만 그래도 구현했음에 의의를...

--- samsung-sds/graph/boj_11438.py
+++ samsung-sds/graph/boj_11438.py
@@ -1,12 +1,13 @@
-# 백준
+# 백준 - LCA 2
 
 
-import sys, collections
+import sys, collections, math
 
 N = int(sys.stdin.readline())
+logN = int(math.log2(N)) + 1
 graph = {i+1 : set() for i in range(N)}
 depths = [0] * (N+1) # 각 노드까지의 깊이
-parents = [0 for _ in range(N+1)]
+parents = [[0 for _ in range(logN)] for _ in range(N+1)] # 각 노드에 대해 2^i번째 부모 정보를 기록
 
 
 
@@ -27,24 +28,42 @@ def find_depth():
 
         for node in graph[now]:
             if not visited[node]:
-                parents[node] = now
+                parents[node][0] = now
                 queue.append([node, depth+1])
+
+def set_parents():
+    find_depth()
+
+
+
+    for i in range(1, logN):
+        for j in range(1, N+1):
+
+            parents[j][i] = parents[parents[j][i-1]][i-1]
+
 
 
 def lca(a,b):
     # 1. a, b의 depth가 동일하도록 조정
-    while depths[a] != depths[b]:
+    if depths[a] != depths[b]:
+        # b가 더 depth가 깊다고 설정
         if depths[a] > depths[b]:
-            a = parents[a]
+            a, b = b, a
 
-        else: b = parents[b]
+    for i in range(logN-1, -1, -1):
+        if depths[b] - depths[a] >= 2**i:
+            b= parents[b][i]
+
+    if a == b: return a
 
     # 2. 노드가 같아질때까지 올라감
-    while a != b:
-        a = parents[a]
-        b = parents[b]
+    for i in range(logN-1, -1, -1):
+        if parents[a][i] != parents[b][i]:
 
-    return a
+            a = parents[a][i]
+            b = parents[b][i]
+
+    return parents[a][0]
 
 
 
@@ -58,7 +77,7 @@ for _ in range(N-1):
 
 # input command
 
-find_depth()
+set_parents()
 
 M = int(sys.stdin.readline())
 for _ in range(M):
