a07da48a0db87eedddc09f99dd2448e26533f095
superyodi <songagreen@gmail.com>
2021-07-28 16:24:33 +0900

    heap 백준 절댓값 힙 문제 해결

---
+++ heap/boj_11286.py
@@ -0,0 +1,23 @@
+# 백준 - 절댓값 힙
+
+
+import sys
+import heapq as hq
+
+
+N = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x != 0:
+        if x < 0:
+            hq.heappush(arr, (x*-1, x))
+        else:
+            hq.heappush(arr, (x, x))
+
+    else:
+        if arr:
+            num = hq.heappop(arr)
+            print(num[1])
+        else:
+            print("0")
