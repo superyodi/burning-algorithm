4997a22bc7add38faafd19c93b9f2faeb1c5bc55
superyodi <songagreen@gmail.com>
2022-01-18 00:08:57 +0900

    dfs N과M 1 문제해결

---
+++ dfs/BOJ15649.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15649 {
+    static int N,M;
+    static boolean[] visited;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        answer = new int[M];
+        visited = new boolean[N];
+        dfs(0,0);
+        System.out.print(sb);
+
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(answer[i]).append(' ');
+            }
+            sb.append('\n');
+        }
+        else {
+            for(int i = 0; i < N; i++) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    answer[depth] = i+1;
+                    dfs(start+1, depth+1);
+                    visited[i] = false;
+                }
+            }
+        }
+
+    }
+}
